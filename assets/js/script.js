7969340e7032a8af57f538bee45830116dc23845
ScoBoFoSho <ferrecisco@icloud.com>
2022-07-23 22:29:10 -0400

    Merge Setup/Add Map to Weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,57 +2,56 @@
 // for the other API calls, I have tried to keep everything in one function so
 // adding code below wont be an issue
 
-
 let weather = {
-    apiKey: "f2da475e3f33a76106dd40ea31ab6cfa",
-    fetchWeather: function (city) {
-        fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=" +
-            city +
-            "&units=imperial&appid=" +
-            this.apiKey
-        )
-            .then((response) => {
-                if (!response.ok) {
-                    alert("No weather found.");
-                    throw new Error("No weather found.");
-                }
-                return response.json();
-            })
-            .then((data) => this.displayWeather(data));
-    },
-    displayWeather: function (data) {
-        const { name } = data;
-        const { icon, description } = data.weather[0];
-        const { temp, humidity } = data.main;
-        const { speed } = data.wind;
-        document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src =
-            "https://openweathermap.org/img/wn/" + icon + ".png";
-        document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°F";
-        document.querySelector(".humidity").innerText =
-            "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText =
-            "Wind speed: " + speed + " km/h";
-        document.querySelector(".weather").classList.remove("loading");
-    },
-    search: function () {
-        this.fetchWeather(document.querySelector(".search-bar").value);
-    },
+  apiKey: "f2da475e3f33a76106dd40ea31ab6cfa",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=imperial&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°F";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
 };
 
 document.querySelector(".search button").addEventListener("click", function () {
-    weather.search();
+  weather.search();
 });
 
 document
-    .querySelector(".search-bar")
-    .addEventListener("keyup", function (event) {
-        if (event.key == "Enter") {
-            weather.search();
-        }
-    });
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
 
 weather.fetchWeather("Miami");
 
@@ -62,3 +61,10 @@ weather.fetchWeather("Miami");
 //If you want to use the same city that is typed into the search bar for the other api use:,
 //document.querySelector(".search-bar").value
 
+// Map Object created!
+var parkMap = L.map("parkMap").setView([38.18, -95.34], 4);
+// uses css link to add sytled tiles to object named "parkMap"
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 16,
+  attribution: "© OpenStreetMap",
+}).addTo(parkMap);
